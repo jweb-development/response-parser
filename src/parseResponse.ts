fdd6de6d5758f5f19cb035b7fed8dfb29e7045b1
SEKCJ <caesarjua@gmail.com>
2021-11-05 14:59:07 -0500

    added check for 403 errors

--- src/parseResponse.ts
+++ src/parseResponse.ts
@@ -13,7 +13,7 @@ const unhandledErrorResponse = {
 export const parseResponse = (response: IRequestResponse, parseOptions: IParseResponse = {}): (IStatuses | IUnhandled) => {
   const { shouldDispatch = false } = parseOptions
 
-  if (response.status && response.status === 401 && shouldDispatch) {
+  if (response && response.status && (response.status === 401 || response.status === 403) && shouldDispatch) {
     // If user is attemtping to hit a non-public endpoint with invalid credentials, log them out
     const { dispatch = () => { }, dispatchType = '', payload = {} } = parseOptions
     dispatch({ type: dispatchType, payload })
