c92de6aa8634c52fbf24d36f782fb6459a6d6865
jjchambl <jjchambliss@gmail.com>
2022-02-03 16:28:38 -0600

    update metadata, add publish workflow, unit tests

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { parseResponse } from './parseResponse';
-import { responseTypes, responses, IParseResponse, IRequestResponse, IDispatch, IStatuses } from './responses';
-export { parseResponse, responseTypes, responses, IParseResponse, IRequestResponse, IDispatch, IStatuses };
--- dist/parseResponse.d.ts
+++
@@ -1,6 +0,0 @@
-import { IRequestResponse, IParseResponse, IStatuses } from './responses';
-export interface IUnhandled {
-    error: Boolean;
-    message: String;
-}
-export declare const parseResponse: (response: IRequestResponse, parseOptions?: IParseResponse) => (IStatuses | IUnhandled);
